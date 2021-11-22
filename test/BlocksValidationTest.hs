37f4575ad3153cd37a7d2bd404b380068903b83b
Zaabson <karolochmanmilarski@gmail.com>
2021-11-23 00:11:53 +0100

    dFix.

--- test/BlocksValidationTest.hs
+++ test/BlocksValidationTest.hs
@@ -4,16 +4,16 @@ import BlockType
 import ArbitraryBlock
 import Test.QuickCheck
 import qualified Data.Map as Map
-import BlockValidation (UTXOPool, validateBlockTransactions,UTXO (UTXO))
+import BlockValidation (UTXOPool(..), validateBlockTransactions,UTXO (UTXO))
 import Data.List (foldl')
 import BlockCreation (OwnedUTXO(OwnedUTXO))
 
 
 foldBlockchain :: [Block] -> Maybe UTXOPool
-foldBlockchain = foldl' f (Just Map.empty)
+foldBlockchain = foldl' f (Just (UTXOPool Map.empty))
     where
-        f (Just utxos) block =
-            case validateBlockTransactions utxos block of
+        f (Just (UTXOPool utxos)) block =
+            case validateBlockTransactions (UTXOPool utxos) block of
                 (True, newUtxos) -> Just newUtxos
                 (False, _ )      -> Nothing
         f Nothing _ = Nothing
@@ -23,7 +23,7 @@ foldBlockchain = foldl' f (Just Map.empty)
 prop_UTXOPoolCorrect :: BlockchainWithState -> Bool
 prop_UTXOPoolCorrect (BlockchainWithState utxos blocks genesis) =
     case foldBlockchain (reverse blocks) of
-        Just utxoPool -> 
+        Just (UTXOPool utxoPool) -> 
             -- all utxoPool contains all utxos and theyre the same size, then utxoPool == utxos
             all (\(OwnedUTXO (UTXO txid vout _) _) -> 
                 (txid, vout) `Map.member` utxoPool) utxos
