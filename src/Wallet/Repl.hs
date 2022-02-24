d74252c6844964abe7d798022765bbd63ff0f0e1
Zaabson <karolochmanmilarski@gmail.com>
2022-02-24 23:09:45 +0100

    Add Wallet.Repl.

---
+++ src/Wallet/Repl.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Wallet.Repl where
+
+import BlockCreation (OwnedUTXO)
+import BlockType (Transaction, TXID, Cent, PublicAddress)
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON, FromJSON, eitherDecode, encode)
+import Server (readAllMessages)
+import Node (Status)
+import qualified Data.ByteString.Lazy as Lazy
+import Data.Bifunctor (second)
+import Network.Socket (Socket)
+import Client (send)
+
+-- Non-GADT type to transform to and from JSON data. 
+data CommandJSON
+    = AddCoinJSON  OwnedUTXO
+    | AddTransactionJSON  Transaction
+    | SendTransactionJSON  PublicAddress Cent
+    | GetStatusJSON  TXID
+    deriving (Generic)
+
+instance ToJSON CommandJSON
+instance FromJSON CommandJSON
+
+-- Type for a command to wallet repl. 
+data CommandR response where
+    AddCoin         :: OwnedUTXO ->             CommandR AddCoinResponse
+    AddTransaction  :: Transaction ->           CommandR AddTransactionResponse
+    SendTransaction :: PublicAddress -> Cent -> CommandR SendTransactionResponse
+    GetStatus       :: TXID ->                  CommandR StatusResponse
+
+-- Existentially qualified CommandR type.
+data Command = forall r . ToJSON r => Command (CommandR r)
+
+parseCommand :: Lazy.ByteString -> Either String Command
+parseCommand = second commandFromJSON . eitherDecode
+    where
+        commandFromJSON :: CommandJSON -> Command
+        commandFromJSON (AddCoinJSON utxo) = Command $ AddCoin utxo
+        commandFromJSON (AddTransactionJSON tx) = Command $ AddTransaction tx
+        commandFromJSON (SendTransactionJSON pubaddr n) = Command $ SendTransaction pubaddr n
+        commandFromJSON (GetStatusJSON txid) = Command $ GetStatus txid
+
+data AddCoinResponse
+    = AddCoinSuccess
+    | AddCoinFail
+    deriving (Generic)
+
+instance ToJSON AddCoinResponse 
+instance FromJSON AddCoinResponse 
+
+data AddTransactionResponse
+    = AddTransactionSuccess
+    | AddTransactionFail
+    deriving (Generic)
+
+instance ToJSON AddTransactionResponse 
+instance FromJSON AddTransactionResponse 
+
+data SendTransactionResponse
+    = SendedTransaction
+    | NotEnoughFunds
+    | SendTransactionFailure
+    deriving (Generic)
+
+instance ToJSON SendTransactionResponse 
+instance FromJSON SendTransactionResponse 
+
+data StatusResponse
+    = StatusIs TXID Status
+    | GetStatusFailure
+    deriving (Generic)
+
+instance ToJSON StatusResponse 
+instance FromJSON StatusResponse 
+
+foo :: CommandR r -> IO r
+foo (AddCoin utxo) = return AddCoinSuccess
+
+-- f _ = undefined
+
+readAllCommands :: Socket -> IO [Either String Command]
+readAllCommands sock = map parseCommand <$> readAllMessages sock
+
+processMessages :: Socket -> (String -> IO ()) -> (forall r . CommandR r-> IO r) -> IO ()
+processMessages sock log f = do 
+    cmds <- readAllCommands sock
+    tillLeft cmds
+    where   
+        -- loops till left error 
+        tillLeft [] = return ()
+        tillLeft (Left str : es) = log ("Command decoding error: \n" <> str)
+        tillLeft (Right (Command c) : es) = do
+            resp <- f c
+            send sock $ encode resp
+            tillLeft es
+            
