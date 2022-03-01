3531d0dbd09d20f530abd89b253799d575b42f53
Zaabson <karolochmanmilarski@gmail.com>
2022-03-01 22:31:28 +0100

    Update launch script.

--- app/launch3nodes.sh
+++ app/launch3nodes.sh
@@ -3,6 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/.." || exit
 
+touch miner-logs1
+touch miner-logs2
+touch wallet-logs
+
 stack run -- miner --config app/configs/miner_config1.yaml &
 stack run -- miner --config app/configs/miner_config2.yaml &
 
