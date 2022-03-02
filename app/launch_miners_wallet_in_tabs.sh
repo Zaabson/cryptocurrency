7f0b885731e550986ae4547f2f7c13ac8e59f270
Zaabson <karolochmanmilarski@gmail.com>
2022-03-02 16:50:02 +0100

    Build before launch.

--- app/launch_miners_wallet_in_tabs.sh
+++ app/launch_miners_wallet_in_tabs.sh
@@ -7,6 +7,8 @@ touch miner-logs1
 touch miner-logs2
 touch wallet-logs
 
+stack build
+
 gnome-terminal --tab -- sh -c 'stack run -- miner --config app/configs/miner_config1.yaml & tail -f miner-logs1'
 gnome-terminal --tab -- sh -c 'stack run -- miner --config app/configs/miner_config2.yaml & tail -f miner-logs2'
 
